8a7e4e61f908d54d056a6623d120998caa5b1383
Vibhor <vibhorsingla3104@gmail.com>
2024-03-10 18:24:44 +0530

    delete functionality done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 import "./App.css";
-import AddRoom from "./components/rooms/AddRoom";
+//import AddRoom from "./components/rooms/AddRoom";
 import ExistingRooms from "./components/rooms/ExistingRooms.jsx";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import EditRoom from "./components/rooms/EditRoom.jsx";
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <AddRoom />
-        <ExistingRooms />
-      </BrowserRouter>
-    </div>
+    <>
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
   );
 }
 
